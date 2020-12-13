9114903b5ce1fdf7433e1ec04892a9c31dc611cf
David Kim <kim.david.j@gmail.com>
2020-12-12 17:11:14 -0800

    Fixed gulp build bug

--- server/src/model/chat-message.ts
+++ server/src/model/chat-message.ts
@@ -1,7 +1,7 @@
 import { Message, User } from './';
 
-export class ChatMessage extends Message{
-    constructor(from: User, content: string) {
-        super(from, content);
+export class ChatMessage extends Message {
+    constructor(from: User, content: string, socketId: string) {
+        super(from, content, socketId);
     }
 }
